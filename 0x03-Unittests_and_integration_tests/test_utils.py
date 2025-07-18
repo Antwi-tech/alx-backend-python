530e52c1489c48d20701721aa7faad1aff84bc38
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-18 19:44:44 +0000

    usin mock in testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """Unit tests for utils module."""
 
 import unittest
-from unittest.mock import patch, Mock
+from unittest.mock import MagicMock, patch, Mock
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
