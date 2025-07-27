d8b14e5d054d9b4c09a4de7c1167c0ebd29426ac
Antwi-tech <nanaafuaantwiwaa624@gmail.com>
2025-07-27 21:02:44 +0000

    middleware

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -103,8 +103,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ), # type: ignore
 
 MIDDLEWARE = [
-    ...
-    'chats.middleware.RequestLoggingMiddleware'
+    # default Django middleware...
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    
+    
+    'chats.middleware.RestrictAccessByTimeMiddleware',
 ]
 
 # DRF + JWT Authentication Settings
